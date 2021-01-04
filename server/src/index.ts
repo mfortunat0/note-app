2b0f7914d7dcbc97c3591ea6fe0ba8fc2dfe4261
matheus <matheus.xmaz10@gmail.com>
2021-01-04 16:51:21 -0300

    Changed paths

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import { ApolloServer } from "apollo-server";
 import resolvers from "./resolvers";
 import fs from "fs";
+import path from "path";
 
-const typeDefs = fs.readFileSync("src/schema.graphql", "utf8").toString();
+const typeDefs = fs
+  .readFileSync(path.join(__dirname, "../schema.graphql"), "utf8")
+  .toString();
 
 const server = new ApolloServer({
   typeDefs,
